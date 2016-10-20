c8711e47f31432ec4bb0774ee311a6296875f5e6
J.W. Clark <jwclark@rockhursths.edu>
2016-10-20 00:28:32 -0500

    Copy from ios site

--- WebClient/main.js
+++ WebClient/main.js
@@ -1,20 +1,34 @@
 $(document).ready(function() {
-    var socket = io.connect();
-    
+    var socketServer = 'http://192.168.1.8/';
+    var socket = io.connect(socketServer);
+
+    socket.on('connect', function() {
+        $('#error').hide();
+        $('#error').html('');
+        $('#noerror').show();
+        console.log('connected :)');
+    });
+
+    socket.io.on('connect_error', function(err) {
+        $('#error').html(err + '<br>Chat server offline');
+        $('#error').show();
+        $('#noerror').hide();
+    });
+
     socket.on('welcome', function(data) {
         $('#log').append('<div><strong>' + data.text + '</strong></div>');
     });
-    
+
     socket.on('otherUserDisconnect', function(data) {
         $('#log').append('<div><strong>' + data + ' disconnected</strong></div>');
     });
-    
+
     socket.on('message', function(data) {
         $('#log').append('<div><strong>' + data.user + ': ' + data.message + '</strong></div>');
     })
-    
+
     var name;
-    
+
     $('#user-save').click(function() {
         console.log('click');
         var username = $('#user-name');
@@ -30,7 +44,7 @@ $(document).ready(function() {
             socket.emit('user', name);
         }
     });
-    
+
     $('#send').click(function() {
         var input = $('#message');
         var text = input.val().trim();
@@ -39,6 +53,6 @@ $(document).ready(function() {
         }
         input.val('');
     });
-    
-    
+
+
 });
